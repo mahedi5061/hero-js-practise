07c6cc7478a83aeb947bb2eeeae17d3f72fe068b
mahedi5061 <mahedi5061@gmail.com>
2021-01-18 20:24:40 +0600

    adding a file

---
+++ arr-fun.js
@@ -0,0 +1,36 @@
+// var arr=[10,20,30,40,50,60,70];
+
+// function func(numbers){
+// for(var i=0;i<numbers.length;i++){
+//     var larger=numbers[0];
+     
+//     if(numbers[i]> larger){
+//         larger=numbers[i];
+//     }
+// }
+
+//      return larger; 
+
+// }
+
+//  var result=func([40,60,90]);
+// console.log(result);
+
+
+var arr=[10,20,30,40,50,60,70];
+
+function func(numbers){
+for(var i=0;i<numbers.length;i--){
+    var larger=numbers[0];
+     
+    if(numbers[i]< larger){
+        larger=numbers[i];
+    }
+}
+
+     return larger; 
+
+}
+
+ var result=func([40,60,90]);
+console.log(result);
